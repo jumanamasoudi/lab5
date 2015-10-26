b15cea72ee89da89ac9725c87b8893081b62c41b
jmasoudi <jumana.masoudi@ryerson.ca>
2015-10-26 16:43:32 -0400

    add new line

---
+++ lab5/src/lab5.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : lab5.cpp
+// Author      : jumana
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Hello again"<< endl;
+	return 0;
+}
